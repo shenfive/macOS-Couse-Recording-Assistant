f657a2835440fd791252436db840d1c8291e8158
Danny Shen <shenfive@hotmail.com>
2021-12-15 21:57:20 +0800

    Initial Commit

---
+++ Couse Recording Assistant/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Couse Recording Assistant
+//
+//  Created by 申潤五 on 2021/12/15.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Couse Recording Assistant/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Couse Recording Assistant
+//
+//  Created by 申潤五 on 2021/12/15.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
